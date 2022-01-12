ffeb22d2ff19f6e8f9be55950c62bcabc5f04b42
Clifford Beshers <Clifford Beshers clifford.beshers@gmail.com>
2022-01-12 14:43:08 -0800

    getElementById, haddock in JS

--- src/SeeReason/DOM/JS.hs
+++ src/SeeReason/DOM/JS.hs
@@ -6,6 +6,7 @@
 module SeeReason.DOM.JS
   ( createElement
   , createTextNode
+  , getElementById
   , body
   , document
   , appendChild
@@ -31,41 +32,51 @@ import qualified GHCJS.DOM.Window as DOM (getHistory)
 import qualified GHCJS.DOM as DOM (currentWindowUnchecked)
 import GHCJS.DOM.Types (FromJSString, ToJSString, FromJSVal(..), ToJSVal(..), Window, toJSString, fromJSString, fromMaybeJSString, castTo)
 
+-- | invokes document.getElementById(ident)
+getElementById :: ToJSString ident => Document -> ident -> DOM Element
+getElementById doc ident = DOM $ do
+  liftIO $ js_getElementById doc (toJSString ident)
 
+foreign import javascript unsafe "$r = ($1).getElementById($2)"
+  js_getElementById :: Document -> JSString -> IO DOM.Element
+
+-- | invokes document.createElement(tag)
+createElement :: ToJSString tag => Document -> tag -> DOM Element
+createElement doc tag = DOM $ do
+  liftIO $ js_createElement doc (toJSString tag)
 foreign import javascript unsafe "$r = ($1).createElement($2)"
   js_createElement :: Document -> JSString -> IO DOM.Element
 
 
-createElement :: ToJSString tag => Document -> tag -> DOM Element
-createElement doc tag = DOM $ do
-  liftIO $ js_createElement doc (toJSString tag)
-
-foreign import javascript unsafe "$r = ($1).createTextNode($2)"
-  js_createTextNode :: Document -> JSString -> IO DOM.Element
-
+-- | invokes document.createTextNode(text)
 createTextNode :: ToJSString t => Document -> t -> DOM Element
 createTextNode doc t = DOM $ do
   liftIO $ js_createTextNode doc (toJSString t)
 
-foreign import javascript unsafe "$r = document"
-  js_document :: IO Document
+foreign import javascript unsafe "$r = ($1).createTextNode($2)"
+  js_createTextNode :: Document -> JSString -> IO DOM.Element
 
+-- | invokes document
 document :: DOM Document
 document = DOM $ do
   liftIO $ js_document
 
+foreign import javascript unsafe "$r = document"
+  js_document :: IO Document
+
+-- | invokes document.body
+body :: Document -> DOM Element
+body = DOM . liftIO . js_body
+
 foreign import javascript unsafe "$r = $1.body"
   js_body :: Document -> IO Element
 
-body :: Document -> DOM Element
-body = DOM . liftIO . js_body
-  
-
-foreign import javascript unsafe "$1.appendChild($2)"
-   js_appendChild :: Element -> Element -> IO ()
-
+-- | invokes parent.appendChild(child)
 appendChild :: Element -> Element -> DOM ()
 appendChild parent child = DOM . liftIO $ do
   js_appendChild parent child
+  
+foreign import javascript unsafe "$1.appendChild($2)"
+   js_appendChild :: Element -> Element -> IO ()
 
 
